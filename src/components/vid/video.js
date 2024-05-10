b40afb93657cda052eee9763d292101138ef430a
wait4theQ <waitforthekyu@gmail.com>
2024-05-10 17:42:34 -0300

    att

--- src/components/vid/video.js
+++ src/components/vid/video.js
@@ -39,7 +39,7 @@ const VideoSection = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
-                        <span>Assista Depois</span>
+                        <span>Não, obrigado(a) vou assistir depois!</span>
                     </div>
                 </div>
             </div>
