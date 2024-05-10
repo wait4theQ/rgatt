8558c3adc323521c68496f305d5a6b33ed154408
wait4theQ <waitforthekyu@gmail.com>
2024-05-10 15:34:35 -0300

    att

--- src/components/vid/video.js
+++ src/components/vid/video.js
@@ -31,15 +31,16 @@ const VideoSection = () => {
         <div className="video-section" style={{ pointerEvents: videoState === 'normal' ? 'auto' : 'none' }}>
             <div className="video-overlay" onClick={handleOverlayClick} style={{ visibility: videoState === 'normal' ? 'visible' : 'hidden', opacity: videoState === 'normal' ? 1 : 0 }}>
                 <div className="video-modal" onClick={(e) => e.stopPropagation()}>
-                    <div className="minimize-icon" onClick={toggleVideoState}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M18 12H6" />
-                        </svg>
-                    </div>
-                    <h3>Assista o vídeo antes de comprar o curso!</h3>
                     <video ref={videoRef} width="100%" height="auto" controls>
                         <source src={videoSrc} type="video/mp4" />
                     </video>
+                    <div className="minimize-icon" onClick={toggleVideoState}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                        <span>Assista Depois</span>
+                    </div>
+                    <h3>Assista o vídeo antes de comprar o curso!</h3>
                 </div>
             </div>
             <button className={`video-minimized ${videoState === 'minimized' ? '' : 'hidden'}`} onClick={toggleVideoState} style={{ pointerEvents: 'auto' }}>
